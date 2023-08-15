284b47c2da54222ae2634a51e441378a0bb9c9b7
Hyeonwoo <leojung0909@gmail.com>
2023-08-15 15:42:38 +0900

    :bug: fix: modify sql delete script

--- src/main/kotlin/community/mingle/mingledemo/domain/member/entity/Member.kt
+++ src/main/kotlin/community/mingle/mingledemo/domain/member/entity/Member.kt
@@ -1,5 +1,7 @@
 package community.mingle.mingledemo.domain.member.entity
 
+import community.mingle.mingledemo.domain.post.entity.Post
+import community.mingle.mingledemo.entitybase.AuditLoggingBase
 import community.mingle.mingledemo.enums.MemberRole
 import community.mingle.mingledemo.enums.MemberStatus
 import jakarta.persistence.*
@@ -9,7 +11,7 @@ import java.time.LocalDateTime
 
 @Entity
 @Where(clause = "deleted_at IS NULL")
-@SQLDelete(sql = "UPDATE department_available_time SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+@SQLDelete(sql = "UPDATE member SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
 @Table(name = "member")
 class Member(
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
@@ -34,20 +36,17 @@ class Member(
     @Column(name = "agreed_at")
     var agreedAt: LocalDateTime? = null,
 
-    @Column(name = "created_at", nullable = false)
-    var createdAt: LocalDateTime,
-
-    @Column(name = "updated_at", nullable = false)
-    var updatedAt: LocalDateTime,
-
     @Column(name = "deleted_at")
     var deletedAt: LocalDateTime? = null,
 
     @Column(name = "fcm_token")
     var fcmToken: String,
-) {
+) : AuditLoggingBase() {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     val id: Long? = null
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
+    var posts = mutableListOf<Post>()
 }
