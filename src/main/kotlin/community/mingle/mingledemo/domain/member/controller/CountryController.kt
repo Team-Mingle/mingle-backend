f7a9cdf41adff2f8a5a3c49e174cce6257061989
Hyeonwoo <leojung0909@gmail.com>
2023-08-15 18:17:35 +0900

    :memo: feat: add swagger document

--- src/main/kotlin/community/mingle/mingledemo/domain/member/controller/CountryController.kt
+++ src/main/kotlin/community/mingle/mingledemo/domain/member/controller/CountryController.kt
@@ -2,6 +2,8 @@ package community.mingle.mingledemo.domain.member.controller
 
 import community.mingle.mingledemo.domain.member.controller.response.CountriesResponse
 import community.mingle.mingledemo.domain.member.facade.CountryFacade
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -11,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController
 class CountryController(
     private val countryFacade: CountryFacade,
 ) {
+    @Operation(
+        summary = "회원가입 화면에서 전체 국가 리스트 조회",
+        responses = [
+            ApiResponse(responseCode = "200", description = "OK"),
+        ],
+    )
     @GetMapping("")
     fun getCountries(): CountriesResponse {
         val countriesName = countryFacade.getAllCountriesName()
