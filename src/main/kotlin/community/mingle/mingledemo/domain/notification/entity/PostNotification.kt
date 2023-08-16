efec5e6269974783d66b7270671fdf937d6e6432
Hyeonwoo <leojung0909@gmail.com>
2023-08-17 01:22:14 +0900

    :bug: fix: remove open statement

--- src/main/kotlin/community/mingle/mingledemo/domain/notification/entity/PostNotification.kt
+++ src/main/kotlin/community/mingle/mingledemo/domain/notification/entity/PostNotification.kt
@@ -5,17 +5,17 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "post_notification")
-open class PostNotification {
+class PostNotification {
     @Id
     @Column(name = "id", nullable = false)
-    open var id: Long? = null
+    var id: Long? = null
 
     @MapsId
     @OneToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "id", nullable = false)
-    open var notification: Notification? = null
+    var notification: Notification? = null
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "post_id", nullable = false)
-    open var post: Post? = null
+    var post: Post? = null
 }
